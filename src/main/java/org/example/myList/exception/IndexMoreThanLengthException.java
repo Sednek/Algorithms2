c175756fb2e0ac8cb727eac58fd38748966ead1c
Sednek <sednek2@yandex.ru>
2023-07-06 21:32:13 +0300

    hw complete

---
+++ src/main/java/org/example/myList/exception/IndexMoreThanLengthException.java
@@ -0,0 +1,7 @@
+package org.example.myList.exception;
+
+public class IndexMoreThanLengthException extends RuntimeException {
+    public IndexMoreThanLengthException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/myList/exception/NotFoundItemException.java
@@ -0,0 +1,7 @@
+package org.example.myList.exception;
+
+public class NotFoundItemException extends RuntimeException{
+    public NotFoundItemException(String message) {
+        super(message);
+    }
+}
